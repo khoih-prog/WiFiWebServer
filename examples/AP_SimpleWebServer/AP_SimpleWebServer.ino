be11189dea79083de1c6b04c01f61871875488a5
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-03-26 23:33:56 -0400

    Add new example AP_SimpleWebServer
    ### Important note:
    To use AP reliably, update WiFiNINA firmware to v1.3.0+ by using FirmwareUpdate program
    See [WiFiNINAFirmwareUpdater](https://www.arduino.cc/en/Tutorial/WiFiNINAFirmwareUpdater) for more information how to proceed.

---
+++ examples/AP_SimpleWebServer/AP_SimpleWebServer.ino
@@ -0,0 +1,284 @@
+/****************************************************************************************************************************
+   AP_SimpleWebServer.ino - Simple Arduino WiFi Web Server LED Blink sample for SAMD21 running WiFiNINA shield
+   For ESP32-based WiFi shields, such as WiFiNINA W101, W102, W13x, etc
+
+   WiFiWebServer is a library for the ESP32-based WiFi shields to run WebServer
+   Forked and modified from ESP8266 https://github.com/esp8266/Arduino/releases
+   Forked and modified from Arduino WiFiNINA library https://www.arduino.cc/en/Reference/WiFiNINA
+   Built by Khoi Hoang https://github.com/khoih-prog/WiFiWebServer
+   Licensed under MIT license
+   Version: 1.0.0
+
+   A simple web server that lets you blink an LED via the web.
+   This sketch will create a new access point (with no password).
+   It will then launch a new server and print out the IP address
+   to the Serial monitor. From there, you can open that address in a web browser
+   to turn on and off the LED on pin 13.
+
+   If the IP address of your board is yourAddress:
+   http://yourAddress/H turns the LED on
+   http://yourAddress/L turns it off
+
+   created 25 Nov 2012
+   by Tom Igoe
+   adapted to WiFi AP by Adafruit
+
+   Version Modified By   Date      Comments
+   ------- -----------  ---------- -----------
+    1.0.0   K Hoang      12/02/2020 Initial coding for SAMD21, Nano 33 IoT, etc running WiFiNINA
+ *****************************************************************************************************************************/
+#define DEBUG_WIFI_WEBSERVER_PORT Serial
+
+#define USE_WIFI_NINA         true
+
+
+#if    ( defined(ARDUINO_SAM_DUE) || defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_MKRWIFI1010) \
+      || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_SAMD_MKRFox1200) || defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310) \
+      || defined(ARDUINO_SAMD_MKRGSM1400) || defined(ARDUINO_SAMD_MKRNB1500) || defined(ARDUINO_SAMD_MKRVIDOR4000) || defined(__SAMD21G18A__) \
+      || defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS) || defined(__SAM3X8E__) || defined(__CPU_ARC__) )
+#if defined(WIFI_USE_SAMD)
+#undef WIFI_USE_SAMD
+#endif
+#define WIFI_USE_SAMD      true
+#endif
+
+#if ( defined(STM32F0) || defined(STM32F1) || defined(STM32F2) || defined(STM32F3)  ||defined(STM32F4) || defined(STM32F7) )
+#if defined(WIFI_USE_STM32)
+#undef WIFI_USE_STM32
+#endif
+#define WIFI_USE_STM32      true
+#endif
+
+#ifdef CORE_TEENSY
+#if defined(__IMXRT1062__)
+#define BOARD_TYPE      "TEENSY 4.0"
+#elif ( defined(__MKL26Z64__) || defined(ARDUINO_ARCH_AVR) )
+#define BOARD_TYPE      "TEENSY LC or 2.0"
+#else
+#define BOARD_TYPE      "TEENSY 3.X"
+#endif
+
+#elif defined(WIFI_USE_SAMD)
+#if defined(ARDUINO_SAMD_ZERO)
+#define BOARD_TYPE      "SAMD Zero"
+#elif defined(ARDUINO_SAMD_MKR1000)
+#define BOARD_TYPE      "SAMD MKR1000"
+#elif defined(ARDUINO_SAMD_MKRWIFI1010)
+#define BOARD_TYPE      "SAMD MKRWIFI1010"
+#elif defined(ARDUINO_SAMD_NANO_33_IOT)
+#define BOARD_TYPE      "SAMD NANO_33_IOT"
+#elif defined(ARDUINO_SAMD_MKRFox1200)
+#define BOARD_TYPE      "SAMD MKRFox1200"
+#elif ( defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310) )
+#define BOARD_TYPE      "SAMD MKRWAN13X0"
+#elif defined(ARDUINO_SAMD_MKRGSM1400)
+#define BOARD_TYPE      "SAMD MKRGSM1400"
+#elif defined(ARDUINO_SAMD_MKRNB1500)
+#define BOARD_TYPE      "SAMD MKRNB1500"
+#elif defined(ARDUINO_SAMD_MKRVIDOR4000)
+#define BOARD_TYPE      "SAMD MKRVIDOR4000"
+#elif defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS)
+#define BOARD_TYPE      "SAMD ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS"
+#elif ( defined(__SAM3X8E__) || (__SAM3X8E__) || (__CPU_ARC__) )
+#define BOARD_TYPE      "SAMD Board"
+#else
+#define BOARD_TYPE      "SAMD Unknown"
+#endif
+
+#elif defined(WIFI_USE_STM32)
+#if defined(STM32F0)
+#define BOARD_TYPE  "STM32F0"
+#elif defined(STM32F1)
+#define BOARD_TYPE  "STM32F1"
+#elif defined(STM32F2)
+#define BOARD_TYPE  "STM32F2"
+#elif defined(STM32F3)
+#define BOARD_TYPE  "STM32F3"
+#elif defined(STM32F4)
+#define BOARD_TYPE  "STM32F4"
+#elif defined(STM32F7)
+#define BOARD_TYPE  "STM32F7"
+#else
+#warning STM32 unknown board selected
+#define BOARD_TYPE  "STM32 Unknown"
+#endif
+#else
+#define BOARD_TYPE      "AVR Mega"
+#endif
+
+//#include <SPI.h>
+//#include <WiFiNINA.h>
+#include <WiFiWebServer.h>
+
+// Important Note: passord length must be 8+ characters or AP creation failed
+char ssid[] = "Nano";             // your network SSID (name)
+char pass[] = "password";        // your network password
+
+int keyIndex = 0;                // your network key Index number (needed only for WEP)
+
+int led =  LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+void printWiFiStatus()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+}
+
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("Access Point Web Server on " + String(BOARD_TYPE));
+
+  pinMode(led, OUTPUT);      // set the LED pin mode
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+  else
+  {
+    Serial.print("Firmware version installed: ");
+    Serial.println(fv);
+  }
+
+  // by default the local IP address of will be 192.168.4.1
+  // you can override it with the following:
+  // WiFi.config(IPAddress(10, 0, 0, 1));
+
+  // print the network name (SSID);
+  Serial.print("Creating access point named: ");
+  Serial.println(ssid);
+
+  // Create open network. Change this line if you want to create an WEP network:
+  // default AP channel = 1
+  uint8_t ap_channel = 2;
+  status = WiFi.beginAP(ssid, pass, ap_channel);
+  
+  //status = WiFi.beginAP(ssid, pass);
+  
+  if (status != WL_AP_LISTENING)
+  {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true);
+  }
+
+  // wait 10 seconds for connection:
+  //delay(10000);
+
+  // start the web server on port 80
+  server.begin();
+
+  // you're connected now, so print out the status
+  printWiFiStatus();
+}
+
+void loop()
+{
+  // compare the previous status to the current status
+  if (status != WiFi.status())
+  {
+    // it has changed update the variable
+    status = WiFi.status();
+
+    if (status == WL_AP_CONNECTED)
+    {
+      // a device has connected to the AP
+      Serial.println("Device connected to AP");
+    } else
+    {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
+    }
+  }
+
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client)
+  {
+    // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected())
+    {
+      // loop while the client's connected
+      if (client.available())
+      {
+        // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+
+        if (c == '\n')
+        {
+          // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0)
+          {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else
+          {
+            // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {
+          // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H"))
+        {
+          digitalWrite(led, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L"))
+        {
+          digitalWrite(led, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
