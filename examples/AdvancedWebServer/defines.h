e91bb1ce7051d6b2abb97a8d17220d70e470238f
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-04-05 13:33:10 -0400

    v1.7.0 to fix Portenta_H7 bug
    ### Releases v1.7.0
    
    1. Fix issue with Portenta_H7 core v2.7.2+. Check [[Portenta_H7] WiFi WebServer extremely slow from core v2.7.2 - v3.0.1 #441](https://github.com/arduino/ArduinoCore-mbed/issues/441)
    2. Rewrite to avoid `multiple-definitions` linker error for multiple-file project
    3. Add example [multiFileProject](examples/multiFileProject) to demo how to avoid `multiple-definitions` linker error for multiple-file project
    4. Update `Packages' Patches`

--- examples/AdvancedWebServer/defines.h
+++ examples/AdvancedWebServer/defines.h
@@ -378,9 +378,7 @@
 
 #include <WiFiWebServer.h>
 
-char ssid[] = "HueNet1";        // your network SSID (name)
-char pass[] = "jenniqqs";        // your network password
-//char ssid[] = "YOUR_SSID";        // your network SSID (name)
-//char pass[] = "12345678";        // your network password
+char ssid[] = "YOUR_SSID";        // your network SSID (name)
+char pass[] = "12345678";        // your network password
 
 #endif    //defines_h
