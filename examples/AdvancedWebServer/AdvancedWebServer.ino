14988f195dcb93119bd2dc4f22f144346feb98b2
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-11-14 01:04:38 -0500

    v1.10.0 to add new features, such as `CORS`
    ### Releases v1.10.0
    
    1. Add new features, such as `CORS`, etc.
    2. Update code and examples
    3. Use `allman astyle` and add `utils`
    4. Update `Packages' Patches`

--- examples/AdvancedWebServer/AdvancedWebServer.ino
+++ examples/AdvancedWebServer/AdvancedWebServer.ino
@@ -1,30 +1,30 @@
 /**************************************************************************************************************************************
   AdvancedWebServer.ino - Simple Arduino web server sample for SAMD21 running WiFiNINA shield
   For any WiFi shields, such as WiFiNINA W101, W102, W13x, or custom, such as ESP8266/ESP32-AT, Ethernet, etc
-  
+
   WiFiWebServer is a library for the ESP32-based WiFi shields to run WebServer
   Forked and modified from ESP8266 https://github.com/esp8266/Arduino/releases
   Forked and modified from Arduino WiFiNINA library https://www.arduino.cc/en/Reference/WiFiNINA
   Built by Khoi Hoang https://github.com/khoih-prog/WiFiWebServer
   Licensed under MIT license
-  
+
   Copyright (c) 2015, Majenko Technologies
   All rights reserved.
-  
+
   Redistribution and use in source and binary forms, with or without modification,
   are permitted provided that the following conditions are met:
-  
+
   Redistributions of source code must retain the above copyright notice, this
   list of conditions and the following disclaimer.
-  
+
   Redistributions in binary form must reproduce the above copyright notice, this
   list of conditions and the following disclaimer in the documentation and/or
   other materials provided with the distribution.
-  
+
   Neither the name of Majenko Technologies nor the names of its
   contributors may be used to endorse or promote products derived from
   this software without specific prior written permission.
-  
+
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,31 +45,31 @@ int reqCount = 0;                // number of requests received
 WiFiWebServer server(80);
 
 #if defined(LED_BUILTIN)
-  const int led =  LED_BUILTIN;
+	const int led =  LED_BUILTIN;
 #else
-  #if (ESP32)
-    // Using pin 13 will crash ESP32_C3
-    const int led =  2;
-  #else
-    const int led =  13;
-  #endif
+	#if (ESP32)
+		// Using pin 13 will crash ESP32_C3
+		const int led =  2;
+	#else
+		const int led =  13;
+	#endif
 #endif
 
 void handleRoot()
 {
 #define BUFFER_SIZE     512
-  
-  digitalWrite(led, 1);
-  char temp[BUFFER_SIZE];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-  int day = hr / 24;
 
-  hr = hr % 24;
+	digitalWrite(led, 1);
+	char temp[BUFFER_SIZE];
+	int sec = millis() / 1000;
+	int min = sec / 60;
+	int hr = min / 60;
+	int day = hr / 24;
 
-  snprintf(temp, BUFFER_SIZE - 1,
-           "<html>\
+	hr = hr % 24;
+
+	snprintf(temp, BUFFER_SIZE - 1,
+	         "<html>\
 <head>\
 <meta http-equiv='refresh' content='5'/>\
 <title>%s</title>\
@@ -86,212 +86,242 @@ body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Col
 </body>\
 </html>", BOARD_NAME, BOARD_NAME, SHIELD_TYPE, day, hr, min % 60, sec % 60);
 
-  server.send(200, F("text/html"), temp);
-  digitalWrite(led, 0);
+	server.send(200, F("text/html"), temp);
+	digitalWrite(led, 0);
 }
 
 void handleNotFound()
 {
-  digitalWrite(led, 1);
-  
-  String message = F("File Not Found\n\n");
-  
-  message += F("URI: ");
-  message += server.uri();
-  message += F("\nMethod: ");
-  message += (server.method() == HTTP_GET) ? F("GET") : F("POST");
-  message += F("\nArguments: ");
-  message += server.args();
-  message += F("\n");
-  
-  for (uint8_t i = 0; i < server.args(); i++)
-  {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  
-  server.send(404, F("text/plain"), message);
-  
-  digitalWrite(led, 0);
+	digitalWrite(led, 1);
+
+	String message = F("File Not Found\n\n");
+
+	message += F("URI: ");
+	message += server.uri();
+	message += F("\nMethod: ");
+	message += (server.method() == HTTP_GET) ? F("GET") : F("POST");
+	message += F("\nArguments: ");
+	message += server.args();
+	message += F("\n");
+
+	for (uint8_t i = 0; i < server.args(); i++)
+	{
+		message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+	}
+
+	server.send(404, F("text/plain"), message);
+
+	digitalWrite(led, 0);
 }
 
-#define ORIGINAL_STR_LEN        2048
 
+#define ORIGINAL_STR_LEN        (2048 * MULTIPLY_FACTOR)
+
+//String out;
+  
 void drawGraph()
 {
-  static String out;
-  static uint16_t previousStrLen = ORIGINAL_STR_LEN;
+	static String out;
+	static uint16_t previousStrLen = ORIGINAL_STR_LEN;
 
-  if (out.length() == 0)
-  {
-    WS_LOGWARN1(F("String Len = 0, extend to"), ORIGINAL_STR_LEN);
-    out.reserve(ORIGINAL_STR_LEN);
-  }
+	if (out.length() == 0)
+	{
+		WS_LOGWARN1(F("String Len = 0, extend to"), ORIGINAL_STR_LEN);
+		out.reserve(ORIGINAL_STR_LEN);
+	}
+
+#if (MULTIPLY_FACTOR == 1)
 
   out = F( "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"310\" height=\"150\">\n" \
            "<rect width=\"310\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"3\" stroke=\"rgb(0, 0, 0)\" />\n" \
+           "<g stroke=\"blue\">\n");  
+                  
+#elif (MULTIPLY_FACTOR == 2)
+
+  out = F( "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"610\" height=\"150\">\n" \
+           "<rect width=\"610\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"3\" stroke=\"rgb(0, 0, 0)\" />\n" \
            "<g stroke=\"blue\">\n");
 
-  char temp[70];
-  
-  int y = rand() % 130;
+#elif (MULTIPLY_FACTOR == 4)
 
-  for (int x = 10; x < 300; x += 10)
-  {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"2\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  
-  out += F("</g>\n</svg>\n");
+  out = F( "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1210\" height=\"150\">\n" \
+           "<rect width=\"1210\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"3\" stroke=\"rgb(0, 0, 0)\" />\n" \
+           "<g stroke=\"blue\">\n");            
 
-  WS_LOGDEBUG1(F("String Len = "), out.length());
+#endif
 
-  if (out.length() > previousStrLen)
-  {
-    WS_LOGERROR3(F("String Len > "), previousStrLen, F(", extend to"), out.length() + 48);
+	char temp[70];
 
-    previousStrLen = out.length() + 48;
-    
-    out.reserve(previousStrLen);
-  }
-  else
-  {
-    server.send(200, "image/svg+xml", out);
-  }
+	int y = rand() % 130;
+
+  for (int x = 10; x < (300 * MULTIPLY_FACTOR); x += 10)
+	{
+		int y2 = rand() % 130;
+		sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"2\" />\n", x, 140 - y, x + 10, 140 - y2);
+		out += temp;
+		y = y2;
+	}
+
+	out += F("</g>\n</svg>\n");
+
+	WS_LOGDEBUG1(F("String Len = "), out.length());
+
+	if (out.length() > previousStrLen)
+	{
+		WS_LOGERROR3(F("String Len > "), previousStrLen, F(", extend to"), out.length() + 48);
+
+		previousStrLen = out.length() + 48;
+
+		out.reserve(previousStrLen);
+	}
+	else
+	{
+		server.send(200, "image/svg+xml", out);
+	}
 }
 
 void setup()
 {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  //out.reserve(ORIGINAL_STR_LEN);
+  
+	pinMode(led, OUTPUT);
+	digitalWrite(led, 0);
 
-  Serial.begin(115200);
-  while (!Serial && millis() < 5000);
+	Serial.begin(115200);
 
-  delay(200);
+	while (!Serial && millis() < 5000);
 
-  Serial.print(F("\nStarting AdvancedWebServer on ")); Serial.print(BOARD_NAME);
-  Serial.print(F(" with ")); Serial.println(SHIELD_TYPE); 
-  Serial.println(WIFI_WEBSERVER_VERSION);
+	delay(200);
+
+	Serial.print(F("\nStarting AdvancedWebServer on "));
+	Serial.print(BOARD_NAME);
+	Serial.print(F(" with "));
+	Serial.println(SHIELD_TYPE);
+	Serial.println(WIFI_WEBSERVER_VERSION);
 
 #if WIFI_USING_ESP_AT
 
-  // initialize serial for ESP module
-  EspSerial.begin(115200);
-  // initialize ESP module
-  WiFi.init(&EspSerial);
+	// initialize serial for ESP module
+	EspSerial.begin(115200);
+	// initialize ESP module
+	WiFi.init(&EspSerial);
 
-  Serial.println(F("WiFi shield init done"));
-  
-#endif  
+	Serial.println(F("WiFi shield init done"));
+
+#endif
 
 #if ! (ESP32 || ESP8266)
-  // check for the presence of the shield
+	// check for the presence of the shield
 #if USE_WIFI_NINA
-  if (WiFi.status() == WL_NO_MODULE)
+
+	if (WiFi.status() == WL_NO_MODULE)
 #else
-  if (WiFi.status() == WL_NO_SHIELD)
+	if (WiFi.status() == WL_NO_SHIELD)
 #endif
-  {
-    Serial.println(F("WiFi shield not present"));
-    // don't continue
-    while (true);
-  }
+	{
+		Serial.println(F("WiFi shield not present"));
+
+		// don't continue
+		while (true);
+	}
+
 #endif
 
 #if USE_WIFI_NINA
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
-  {
-    Serial.println(F("Please upgrade the firmware"));
-  }
+	String fv = WiFi.firmwareVersion();
+
+	if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+	{
+		Serial.println(F("Please upgrade the firmware"));
+	}
+
 #endif
 
 #if (ESP32 || ESP8266)
-    WiFi.mode(WIFI_STA);
+	WiFi.mode(WIFI_STA);
 
-    Serial.print(F("Connecting to WPA SSID: "));
-    Serial.println(ssid);
+	Serial.print(F("Connecting to WPA SSID: "));
+	Serial.println(ssid);
 
-    if (WiFi.status() != WL_CONNECTED)
-    {
-      if (strlen(pass) >= 8)
-      {
-        WiFi.begin(ssid, pass);
-      } 
-      else
-      {
-        WiFi.begin(ssid);
-      }
-    }
-    
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      delay(500);
-    }
+	if (WiFi.status() != WL_CONNECTED)
+	{
+		if (strlen(pass) >= 8)
+		{
+			WiFi.begin(ssid, pass);
+		}
+		else
+		{
+			WiFi.begin(ssid);
+		}
+	}
+
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+	}
 
 #else
 
-  // attempt to connect to WiFi network
-  while ( status != WL_CONNECTED)
-  {
-    //delay(500);
-    Serial.print(F("Connecting to WPA SSID: "));
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network
-    status = WiFi.begin(ssid, pass);
-  }
+	// attempt to connect to WiFi network
+	while ( status != WL_CONNECTED)
+	{
+		//delay(500);
+		Serial.print(F("Connecting to WPA SSID: "));
+		Serial.println(ssid);
+		// Connect to WPA/WPA2 network
+		status = WiFi.begin(ssid, pass);
+	}
+
 #endif
 
-  server.on(F("/"), handleRoot);
-  server.on(F("/test.svg"), drawGraph);
-  server.on(F("/inline"), []()
-  {
-    server.send(200, F("text/plain"), F("This works as well"));
-  });
+	server.on(F("/"), handleRoot);
+	server.on(F("/test.svg"), drawGraph);
+	server.on(F("/inline"), []()
+	{
+		server.send(200, F("text/plain"), F("This works as well"));
+	});
 
-  server.onNotFound(handleNotFound);
-  
-  server.begin();
-  
-  Serial.print(F("HTTP server started @ "));
-  Serial.println(WiFi.localIP());
+	server.onNotFound(handleNotFound);
+
+	server.begin();
+
+	Serial.print(F("HTTP server started @ "));
+	Serial.println(WiFi.localIP());
 }
 
 void heartBeatPrint()
 {
-  static int num = 1;
+	static int num = 1;
 
-  Serial.print(F("H"));
+	Serial.print(F("H"));
 
-  if (num == 80)
-  {
-    Serial.println();
-    num = 1;
-  }
-  else if (num++ % 10 == 0)
-  {
-    Serial.print(F(" "));
-  }
+	if (num == 80)
+	{
+		Serial.println();
+		num = 1;
+	}
+	else if (num++ % 10 == 0)
+	{
+		Serial.print(F(" "));
+	}
 }
 
 void check_status()
 {
-  static unsigned long checkstatus_timeout = 0;
+	static unsigned long checkstatus_timeout = 0;
 
 #define STATUS_CHECK_INTERVAL     60000L
 
-  // Send status report every STATUS_REPORT_INTERVAL (60) seconds: we don't need to send updates frequently if there is no status change.
-  if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
-  {
-    heartBeatPrint();
-    checkstatus_timeout = millis() + STATUS_CHECK_INTERVAL;
-  }
+	// Send status report every STATUS_REPORT_INTERVAL (60) seconds: we don't need to send updates frequently if there is no status change.
+	if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
+	{
+		heartBeatPrint();
+		checkstatus_timeout = millis() + STATUS_CHECK_INTERVAL;
+	}
 }
 
 void loop()
 {
-  server.handleClient();
-  check_status();
+	server.handleClient();
+	check_status();
 }
